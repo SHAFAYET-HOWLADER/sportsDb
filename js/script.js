af5b84cde0aff3d090f5c23ecf05ee4c90c3c203
unknown <you@example.com>
2022-02-26 23:43:57 +0600

    comited

--- js/script.js
+++ js/script.js
@@ -5,32 +5,41 @@ const allPlayers = () =>{
     const inputValue = input.value;
     //clear value
     input.value= "";
+    if(inputValue === "" || inputValue <= 0 ){
+     alert("error")
+    }
+   else{
     //load data by fetching
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${inputValue}`;
     fetch(url)
     .then(res => res.json())
     .then(data =>displayPlayer(data.player))
+  }
 }
 
 // display player
 const displayPlayer = (players) =>{
   const display = document.getElementById("displayPlayerCard");
   display.textContent = "";
-  for(const player of players){
-      console.log(player)
-    const div = document.createElement("div");
-    div.classList.add("stylePlayers");
-    div.innerHTML = `
-    <img src="${player.strThumb}">
-    <h3> Name : ${player.strPlayer}</h3>
-    <h3>Gender : ${player.strGender}</h3>
-    <p> Id : ${player.idPlayer}</p>
-    <button onclick="seeDetails('${player.idPlayer}')">See Details</button>
-    `
-    display.appendChild(div)
+  if(players === null){
+    alert("red")
   }
-}
 
+else{
+    for(const player of players){
+        const div = document.createElement("div");
+        div.classList.add("stylePlayers");
+        div.innerHTML = `
+        <img src="${player.strThumb}">
+        <h3> Name : ${player.strPlayer}</h3>
+        <h3>Gender : ${player.strGender}</h3>
+        <p> Id : ${player.idPlayer}</p>
+        <button onclick="seeDetails('${player.idPlayer}')">See Details</button>
+        `
+        display.appendChild(div)
+      }
+}
+}
 const seeDetails = (unique) =>{
    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${unique}`
    fetch(url)
@@ -47,7 +56,7 @@ const displaySeeDetails = (single) =>{
   <img width="200px" src="${single.strThumb}">
   <h3> Name : ${single.idPlayer}</h3>
   <h3>Gender : ${single.strGender}</h3>
-  <p> Id : ${single.strDescriptionEN.slice(0, 100)}</p>
+  <p> Id : ${single.strDescriptionEN}</p>
   `
   show.appendChild(div)
 }
