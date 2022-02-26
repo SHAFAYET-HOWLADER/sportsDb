bc0fd2c139285200c499db5df1c31ca5179a27cc
unknown <you@example.com>
2022-02-26 21:34:21 +0600

    second commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// allPlayers
+const allPlayers = () =>{
+    const input = document.getElementById("inputField");
+    const inputValue = input.value;
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${inputValue}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
